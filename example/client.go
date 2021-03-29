f42887e50049d0fde8c20c21a342bdbea125a388
Rishabh Nrupnarayan <aryannr97@gmail.com>
2021-03-29 11:45:51 +0530

    [DATA_SERVER]: Example file added for usage

--- example/client.go
+++ example/client.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/aryannr97/data-server/pkg/grpc/ping"
+	"github.com/aryannr97/data-server/pkg/grpc/user"
 	"google.golang.org/grpc"
 )
 
@@ -19,9 +20,36 @@ func main() {
 	client := ping.NewPingServiceClient(conn)
 
 	res, err := client.Ping(context.Background(), &ping.PingRequest{Ping: "status"})
-	if err != nil {
-		log.Printf("Error returned: %v", err)
-	}
+	log.Printf("Server Status: %v Error: %v", res, err)
 
-	log.Printf("Server Status: %v", res.Status)
+	userClient := user.NewUserServiceClient(conn)
+
+	res1, err := userClient.AddUser(context.Background(), &user.UserAddRequest{
+		Firstname: "John",
+		Lastname:  "Doe",
+		Email:     "john@mail.com",
+		Phone:     "4567891245",
+		Country:   "US",
+	})
+	log.Printf("AddUser Response: %v Error: %v", res1, err)
+
+	res2, err := userClient.GetUser(context.Background(), &user.UserGetReuqest{Id: res1.Id})
+	log.Printf("GetUser Response: %v Error: %v", res2, err)
+
+	_, err = userClient.UpdateUser(context.Background(), &user.UserUpdateRequest{
+		Id:        res1.Id,
+		Firstname: "Mike",
+		Lastname:  "Doe",
+		Email:     "mike@mail.com",
+		Phone:     "4567891245",
+		Country:   "US",
+	})
+	log.Printf("UpdateUser Error: %v", err)
+
+	res2, err = userClient.GetUser(context.Background(), &user.UserGetReuqest{Id: res1.Id})
+	log.Printf("GetUser after update Response: %v Error: %v", res2, err)
+	_, err = userClient.DeleteUser(context.Background(), &user.UserDeleteRequest{Id: res1.Id})
+	log.Printf("DeleteUser Error: %v", err)
+	res2, err = userClient.GetUser(context.Background(), &user.UserGetReuqest{Id: res1.Id})
+	log.Printf("GetUser after delete Response: %v Error: %v", res2, err)
 }
